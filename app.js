a95555ef9c635fdee366ef2c939d6f310f646603
Janeth López <janethlh_19@outlook.com>
2025-03-18 02:51:48 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-console.log("Ejecutado el sistema");
+let listaDeAmigos = [];
+let amigoSecreto = 0;
+
+function agregarAmigo() {
+    let amigo = document.getElementById('amigo').value.trim();
+
+    if (amigo == "") {
+        alert("Por favor, inserte un nombre");
+    }if (listaDeAmigos.includes(amigo)) {
+        alert("El nombre ya esta en la lista");
+    } else {
+        listaDeAmigos.push(amigo);
+        limpiarCaja();
+        mostrarLista();
+    } 
+    return;
+}
+
+function limpiarCaja() {
+    document.getElementById('amigo').value = '';
+}
+
+function mostrarLista() {
+    
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for (let i = 0; i < 4; i++) {
+        let li = document.createElement("li");
+        li.textContent = listaDeAmigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function sortearAmigo() {
+    if (listaDeAmigos.length < 2) {
+        alert('Debes ingresar al menos 2 nombres');
+    } else {
+        let index = Math.floor(Math.random() * listaDeAmigos.length);
+        amigoSecreto = listaDeAmigos[index];
+    }
+    console.log(amigoSecreto);
+    let elementoHTML = document.getElementById('resultado');
+    elementoHTML.innerHTML = `<li>Tu amigo secreto es: ${amigoSecreto}!!</li>`
+}
+
